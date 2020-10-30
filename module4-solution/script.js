316b6e7f5d2a215a846ac1912cb4bfb09eebe28f
mariayaneva <m.yaneva4@gmail.com>
2020-10-30 11:18:38 +0200

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -2,6 +2,7 @@
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
+
 for (var i = 0; i < names.length; i++) {
 
   var firstLetter = names[i].charAt(0).toLowerCase();
